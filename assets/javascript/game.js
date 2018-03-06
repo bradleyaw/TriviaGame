d7a10124ac48c11358892e67d1bf53eee806c765
unknown <bradleyaw@gmail.com>
2018-03-05 23:02:03 -0500

    90% Complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,80 +1,72 @@
-/*
-Where does Dwight claim he has buried a large sum of cash?
-Under The Office, Under a body, Under his farm
+var Qs = [{
+    question: "Where does Dwight claim he has buried a large sum of cash?",
+    alternatives: ["under a body", "under the warehouse", "under his barn"],
+    correct: 1,
+}, {
+    question: "Which one of these is not a nickname of Andy's?",
+    alternatives: ["Nard Dog", "Dandy Andy", "Boner Champ"],
+    correct: 2,
+}, {
+    question: "Michael reported to each of these people at Dunder Mifflin except:",
+    alternatives: ["Dwight", "Jim", "Ryan"],
+    correct: 1,
+}, {
+    question: "Dwight is jealous of Jim for the following facial feature?",
+    alternatives: ["his nose", "his cheek bones", "his smile"],
+    correct: 1,
+}]
 
-Dwight is jealous of Jim for the following facial feature?
-His nose, his cheek bones, his smile
-*/
-
-/*Pseudo code
-var qWrong
-var qRight
-var noAnswer
-
-Start button - on click function
-
-function - replace questions with vars displaying
-button to reload page
-
-set timeout(function time)
-set interval time--
-
-done button - setTimeout to 0
-
-*/
 var qWrong = 0;
 var seconds = 30;
 var qRight = 0;
 var noAnswer = 0;
-var questions = '<h2>' + 'Where does Dwight claim he has buried a large sum of cash:' + '</h2>' +
-                '<div class="form-check form-check-inline">' +
-                    '<input class="form-check-input" type="checkbox" id="inlineCheckbox1" name="q1" value="option1">' +
-                    '<label class="form-check-label" for="inlineCheckbox1">Under a body</label>' +
-                '</div>' +
-                '<div class="form-check form-check-inline">' +
-                    '<input class="form-check-input" type="checkbox" id="inlineCheckbox2" name="q1" value="option2">' +
-                    '<label class="form-check-label" for="inlineCheckbox2">Under the office</label>' +
-                '</div>' +
-                '<div class="form-check form-check-inline">' +
-                    '<input class="form-check-input" type="checkbox" id="inlineCheckbox3" name="q1" value="option3">' +
-                    '<label class="form-check-label" for="inlineCheckbox2">under his barn</label>' +
-                '</div>' +
-                '<div><button class="button2">Done</button></div>'
+var questionText;
 
-function questionInsert(){
-    $(".questionCont").html(questions)
+function questionInsert() {
+    $(".questionCont").prepend(questionText)
 }
 
-$(".button").on("click", function(){
+$(".button").on("click", function () {
+    $(".questionCont").html("&nbsp")
     var timer = setTimeout(function () {
         alert("5 seconds have passed");
     }, 5 * 1000);
-    var timer2 = setInterval(decrement, 1000) 
+    var timer2 = setInterval(decrement, 1000)
     function decrement() {
-        seconds--;
-        $(".countdown").html(seconds);
+        if (seconds > 0) {
+            seconds--;
+            $(".countdown").html(seconds);
+        }
+        else {
+            $(".questionCont").html('Questions answered correctly: ' + qRight + "<br>" +
+                'Questions answered incorrectly: ' + qWrong + "<br>" +
+                'Questions left unanswered: ' + noAnswer + "<br>")
+        }
     }
-    $(".questionCont").html(questionInsert());
+    for (var i = 0; i < Qs.length; i++) {
+        questionText = '<h2>' + Qs[i].question + '</h2>' +
+            '<div class="form-check form-check-inline">' +
+                '<input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">' +
+                '<label class="form-check-label" for="inlineCheckbox1">' + Qs[i].alternatives[0] + '</label>' +
+            '</div>' +
+            '<div class="form-check form-check-inline">' +
+                '<input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">' +
+                '<label class="form-check-label" for="inlineCheckbox1">' + Qs[i].alternatives[1] + '</label>' +
+            '</div>' +
+            '<div class="form-check form-check-inline">' +
+                '<input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3">' +
+                '<label class="form-check-label" for="inlineCheckbox1">' + Qs[i].alternatives[2] + '</label>' +
+            '</div>'
+
+        questionInsert();
+    }
+    $(".questionCont").append('<div><button class="button2">Done</button></div>')
     $(".button2").on("click", function () {
         console.log("Hey");
         clearTimeout(timer);
         clearInterval(timer2);
+        $(".questionCont").html('Questions answered correctly: ' + qRight + "<br>" +
+                                'Questions answered incorrectly: ' + qWrong + "<br>" +
+                                'Questions left unanswered: ' + noAnswer)
     });
 });
-
-
-
-
-
-/*
-var 1right = document.getElementById("1right").checked;
-var 1wrong = document.getElementById("1wrong").checked;
-
-if (1right === true) {
-    qRight++
-} else if (1wrong === true) {
-    qwrong++
-} else {
-    noAnswer++
-}
-*/
