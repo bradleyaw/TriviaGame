708519dbc0d8363dc7372059fd4aa19c345b4913
unknown <bradleyaw@gmail.com>
2018-03-14 20:16:42 -0400

    Updated score functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,37 @@
+// Array of Questions and answers
 var Qs = [{
     question: "Where does Dwight claim he has buried a large sum of cash?",
     alternatives: ["under a body", "under the warehouse", "under his barn"],
-    correct: 1,
+    correct: "under a body",
 }, {
     question: "Which one of these is not a nickname of Andy's?",
     alternatives: ["Nard Dog", "Dandy Andy", "Boner Champ"],
-    correct: 2,
+    correct: "Dandy Andy",
 }, {
     question: "Michael reported to each of these people at Dunder Mifflin except:",
     alternatives: ["Dwight", "Jim", "Ryan"],
-    correct: 1,
+    correct: "Dwight",
 }, {
     question: "Dwight is jealous of Jim for the following facial feature?",
     alternatives: ["his nose", "his cheek bones", "his smile"],
-    correct: 1,
+    correct: "his nose",
 }]
+
 
 var qWrong = 0;
 var seconds = 30;
 var qRight = 0;
 var noAnswer = 0;
 var questionText;
-
-function questionInsert() {
-    $(".questionCont").prepend(questionText)
-}
+var panel = $(".questionCont");
 
 $(".button").on("click", function () {
     $(".questionCont").html("&nbsp")
     var timer = setTimeout(function () {
         $(".questionCont").html('Questions answered correctly: ' + qRight + "<br>" +
-                                'Questions answered incorrectly: ' + qWrong + "<br>" +
-                                'Questions left unanswered: ' + noAnswer + "<br>")
+            'Questions answered incorrectly: ' + qWrong + "<br>" +
+            'Questions left unanswered: ' + noAnswer + "<br>")
+            Check();
     }, 30 * 1000);
     var timer2 = setInterval(decrement, 1000)
     function decrement() {
@@ -41,28 +41,58 @@ $(".button").on("click", function () {
         }
     }
     for (var i = 0; i < Qs.length; i++) {
-        questionText = '<h2>' + Qs[i].question + '</h2>' +
-            '<div class="form-check form-check-inline">' +
-                '<input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">' +
-                '<label class="form-check-label" for="inlineCheckbox1">' + Qs[i].alternatives[0] + '</label>' +
-            '</div>' +
-            '<div class="form-check form-check-inline">' +
-                '<input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">' +
-                '<label class="form-check-label" for="inlineCheckbox1">' + Qs[i].alternatives[1] + '</label>' +
-            '</div>' +
-            '<div class="form-check form-check-inline">' +
-                '<input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3">' +
-                '<label class="form-check-label" for="inlineCheckbox1">' + Qs[i].alternatives[2] + '</label>' +
-            '</div>'
-
-        questionInsert();
+        panel.append('<h2>' + Qs[i].question + '</h2>');
+        for (var j = 0; j < Qs[i].alternatives.length; j++) {
+            panel.append("<input type='radio' name='question-" + i +
+                "' value='" + Qs[i].alternatives[j] + "''>" + Qs[i].alternatives[j]);
+        }
     }
     $(".questionCont").append('<div><button class="button2">Done</button></div>')
     $(".button2").on("click", function () {
+        Check();
         clearTimeout(timer);
         clearInterval(timer2);
         $(".questionCont").html('Questions answered correctly: ' + qRight + "<br>" +
-                                'Questions answered incorrectly: ' + qWrong + "<br>" +
-                                'Questions left unanswered: ' + noAnswer)
+            'Questions answered incorrectly: ' + qWrong + "<br>" +
+            'Questions left unanswered: ' + noAnswer)
     });
 });
+
+function Check() {
+    $.each($("input[name='question-0']:checked"), function () {
+        if ($(this).val() === Qs[0].correct) {
+            qRight++;
+        }
+        else {
+            qWrong++;
+        }
+    });
+    $.each($("input[name='question-1']:checked"), function () {
+        if ($(this).val() === Qs[1].correct) {
+            qRight++;
+        }
+        else {
+            qWrong++;
+        }
+    });
+    $.each($("input[name='question-2']:checked"), function () {
+        if ($(this).val() === Qs[2].correct) {
+            qRight++;
+        }
+        else {
+            qWrong++;
+        }
+    });
+    $.each($("input[name='question-3']:checked"), function () {
+        if ($(this).val() === Qs[3].correct) {
+            qRight++;
+        }
+        else {
+            qWrong++;
+        }
+    });
+
+    if ((qRight + qWrong) < Qs.length) {
+        noAnswer = Qs.length - qRight - qWrong;
+    }
+}
